c1802b402ef992ea65bbd45a3da2c7d77e87f645
Dzhamalov Gazimagomed <unixshaman@gmail.com>
2023-05-22 01:32:29 +0300

    Fix config location

--- CerediraJavaAutomation/etc/install/CerediraJavaAutomation/InstallComponent.bat
+++ CerediraJavaAutomation/etc/install/CerediraJavaAutomation/InstallComponent.bat
@@ -8,19 +8,20 @@ for %%i in (../../..) do set devopsat_path=%%~fxi
 echo %devopsat_path%
 echo %devopsat_path:\=/%
 
-copy /y ".\etc\IntelliJIDEA\DontShowAgainFeedbackService.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\encoding.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\grazie_global.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\ide.general.local.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\laf.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\settingsSync.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\shared-indexes.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\updates.xml" "%devopsat_path%\etc\IntelliJIDEA\"
-copy /y ".\etc\IntelliJIDEA\vcs.xml" "%devopsat_path%\var\IntelliJIDEA\"
-
-copy /y ".\var\IntelliJIDEA\ru-6.0.jar" "%devopsat_path%\etc\IntelliJIDEA\"
-
 copy /y ".\bin\IntelliJIDEA\bin\idea.properties" "%devopsat_path%\bin\IntelliJIDEA\bin\"
+
+mkdir "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\DontShowAgainFeedbackService.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\encoding.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\grazie_global.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\ide.general.local.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\laf.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\settingsSync.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\shared-indexes.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\updates.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+copy /y ".\etc\IntelliJIDEA\options\vcs.xml" "%devopsat_path%\etc\IntelliJIDEA\options"
+
+copy /y ".\var\IntelliJIDEA\grazie\ru-6.0.jar" "%devopsat_path%\var\IntelliJIDEA\grazie"
 
 powershell -command "(Get-Content -Raw '.\etc\IntelliJIDEA\git.xml').replace('C:\Ceredira2\CerediraAutomation', '%devopsat_path%') | Set-Content '%devopsat_path%\etc\IntelliJIDEA\git.xml'"
 
