4fa2286394f4fdaace9c48b8f8fb91c8e30bc411
Dzhamalov Gazimagomed <unixshaman@gmail.com>
2023-05-22 01:32:28 +0300

    Fix context menu

--- CerediraAdministration/etc/install/CerediraAdministration/CreateContextMenu.bat
+++ CerediraAdministration/etc/install/CerediraAdministration/CreateContextMenu.bat
@@ -13,6 +13,11 @@ rem Получаем имя текущего каталога исходя из 
 for %%i in (.) do set current_folder_path=%%~nxi
 echo Текущий каталог: %current_folder_path%
 
+rem Получаем имя текущего каталога исходя из каталога запуска данного скрипта
+for %%i in (%~dp0\..\..\..\.) do set root_name=%%~nxi
+echo Имя корневого каталога: %root_name%
+set "current_folder_path=%root_name%_%current_folder_path%"
+echo Имя узла в контекстном меню: %current_folder_path%
 
 set "root=HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\%current_folder_path%"
 
